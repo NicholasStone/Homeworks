c2e3b8459c53d1a1a95fb4f301c532bfd9d2b57b
Nicholas <1264398973@qq.com>
2015-11-13 09:27:18 +0800

    3.31雏形

--- DataStructure/DataStructure/Chapter-3/3.31.cpp
+++ DataStructure/DataStructure/Chapter-3/3.31.cpp
@@ -1,3 +1,33 @@
 //判断以@结尾的任意长字符串是否为回文
 #include<iostream>
 using namespace std;
+
+typedef char Elemtype;
+typedef int Status;
+
+const Status OK = 1;
+const Status ERROR = 0;
+typedef struct Node{
+	Elemtype value;
+	struct Node * next;
+}Node, *LinkList;
+
+LinkList NodeInit( LinkList & L, Elemtype* data )
+{
+	L = new Node;
+	LinkList p = L;
+
+	for(int i = 0; data[ i ]; i++)
+	{
+		p->value = data[ i ];
+		p->next = new Node;
+		p = p->next;
+	}
+	return p;
+}
+bool SymmetryString( Node & L ,Elemtype* data)
+{
+	LinkList Head, End;
+	End = NodeInit( Head ,data);
+
+}
