5c69679fffc725040ca85dc83b0416a803e22f2d
Nicholas <1264398973@qq.com>
2015-12-11 15:39:22 +0800

    调整目录结构

--- Java/JavaExamples/CH02/CH04/src/Ex4_11_testContinue.java
+++
@@ -1,9 +0,0 @@
-public class Ex4_11_testContinue{
-	public static void main(String args[]){
-		for (int i = 0; i < 100; i++) {
-			if ((i%3) ==0)
-				continue;
-			System.out.print(i + "\t");
-		}
-	}
-}
--- Java/JavaExamples/CH02/TEST/src/number.java
+++
@@ -1,8 +0,0 @@
- import   java.text.*;   
-  public   class   number   {   
-      public   static   void   main(String   args[])   {   
-          double   d   =   3.1415926;   
-          DecimalFormat   df   =   new   DecimalFormat("#####.00");   
-          System.out.println(df.format(d));   
-          }   
-      } 
